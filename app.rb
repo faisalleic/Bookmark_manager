8591721019c292b63b023d3ad80d79b82d5199d6
Student <student@macbook-pro-5.home>
2019-06-19 21:08:58 +0100

    bookmarks viewable as a list

--- app.rb
+++ app.rb
@@ -5,6 +5,17 @@ require 'sinatra'
 class BookmarkManager < Sinatra::Base
 
   get '/' do
-    "Hello World"
+    "Bookmark Manager"
   end
+  
+  get '/bookmarks' do
+    @bookmarks = [
+      'http://www.google.co.uk',
+      'http://www.youtube.com',
+      'http://www.yahoo.com'
+    ]
+    erb :'bookmarks/index'
+  end
+
+  run! if app_file == $0
 end
--- spec/feature/bookmark_spec.rb
+++ spec/feature/bookmark_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
 
-feature 'view bookmark ' do
-  scenario 'see hello world' do
+feature 'view homepage ' do
+  scenario 'visit the home page' do
     visit '/'
-    expect(page).to have_content 'Hello World'
+    expect(page).to have_content 'Bookmark Manager'
+  end
+
+  scenario 'view bookmarks' do
+    visit '/bookmarks'
+    expect(page).to have_content 'http://www.google.co.uk'
+    expect(page).to have_content 'http://www.youtube.com'
+    expect(page).to have_content 'http://www.yahoo.com'
   end
 end
